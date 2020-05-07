d911aa1213f7fc99349addedf478f5307db9305b
Thiago Aquino <aquinolima.thiago@gmail.com>
2020-05-07 17:19:30 -0300

    Módulo D - Aula 11

---
+++ Módulo D - Condições em JavaScript/ex008.js
@@ -0,0 +1,13 @@
+// Condição Simples
+
+
+console.log('o console funcionou corretamente!');
+
+var vel = 59
+console.log(`A velocidade do seu carro é de ${vel} Km/h`);
+
+if (vel >60) {
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`);
+}
+
+console.log(`Dirija sempre usando cinto de segurança!`);
